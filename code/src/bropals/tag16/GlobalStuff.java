82f84085cea91413dd97aaef0fce8594fd880871
Kevin <kprehn01@gmail.com>
2014-11-08 13:22:31 -0600

    Global stuff

---
+++ code/src/bropals/tag16/GlobalStuff.java
@@ -0,0 +1,31 @@
+
+public class GlobalStuff {
+	
+	/**
+		How much scrap the player is holding
+	*/
+	private static int scrapAmount = 0;
+	
+	/**
+		The references to the Player entitiy and the Robot entity
+	*/
+	private Entity playerEntity = null, robotEntity = null;
+	/**
+		A boolean value that says whether or not the player is 
+		in combat. The player is in combat when he is being chased
+		by an enemy or in line of sight of an enemy.
+	*/
+	private boolean inCombat = false;
+	
+	public int getScrapAmount() { return scrapAmount; }
+	public void setScrapAmount(int s) { scrapAmount = s; }
+	
+	public Entity getPlayerEntity() { return playerEntity; }
+	public void setPlayerEntity(Entity e) { playerEntity = e; }
+	
+	public Entity getRobotEntity() { return robotEntity; }
+	public void setRobotEntity(Entity e) { robotEntity = e; }
+	
+	public boolean isInCombat() { return inCombat; }
+	public void setInCombat(boolean c) { inCombat = c; }
+}
